4a234433346cc9a2f3f5fb291ed26cf4c2648e5e
CastroOlympias <blackcarrera@msn.com>
2021-05-25 18:40:26 -0600

    created the CRUD db.query to show full table, show one id, delete a row and insert a row

--- server.js
+++ server.js
@@ -20,10 +20,39 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 )
 
-
+// // Query for full list of candidates
 db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 })
+
+// // Query for a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+})
+
+// // Delete a condidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
+})
+
+// Create a condidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, results) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(results);
+});
 
 
 // Default (catchall) response for any other request (NOT FOUND)
