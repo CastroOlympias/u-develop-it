289e42c574fef5dca8cf9eac6669fb1dbbfa96f9
CastroOlympias <blackcarrera@msn.com>
2021-05-27 17:38:55 -0600

    added voters table

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS candidates;
-DROP TABLE IF EXISTS parties;
 DROP DATABASE IF EXISTS election;
 CREATE DATABASE election;
 USE election;
+-- DROP TABLE IF EXISTS candidates;
+-- DROP TABLE IF EXISTS parties;
+
 
 SOURCE db/schema.sql
 SOURCE db/seeds.sql
