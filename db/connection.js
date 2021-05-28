b8a7333e778e549cdda1773dd15d3f4eae2300ae
CastroOlympias <blackcarrera@msn.com>
2021-05-27 18:29:17 -0600

    Added routers for candidates table and parties table, to index.js route hub, also imported into the server.js file

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '1=Brown+Soda',
+        database: 'election'
+    },
+);
+
+module.exports = db;
