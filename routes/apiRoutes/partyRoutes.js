700fa9730059e7cf5e62a92c52332e790b5616eb
CastroOlympias <blackcarrera@msn.com>
2021-05-27 19:29:17 -0600

    finished adding voter endpoints for creating a new voter, update an existing voter an then delete a voter

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
-
+const inputCheck = require('../../utils/inputCheck');
 
 
 // Query All Parties routes
