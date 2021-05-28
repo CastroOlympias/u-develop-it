ad15494e2242c58efc9f17e8277728eaf639e121
CastroOlympias <blackcarrera@msn.com>
2021-05-27 20:15:25 -0600

    added a table to cast votes and use FOREIGN keys for candidates and the voters table

--- db/db.sql
+++ db/db.sql
@@ -1,9 +1,6 @@
 DROP DATABASE IF EXISTS election;
 CREATE DATABASE election;
 USE election;
--- DROP TABLE IF EXISTS candidates;
--- DROP TABLE IF EXISTS parties;
 
-
-SOURCE db/schema.sql
-SOURCE db/seeds.sql
+SOURCE db/schema.sql;
+SOURCE db/seeds.sql;
